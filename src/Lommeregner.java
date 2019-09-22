7fa21cc43d7ca46488dcb54b66d9be6d9890d77c
HFrejlev <henrikfrejlev@hotmail.com>
2019-09-22 20:16:22 +0200

    Initial commit

---
+++ src/Lommeregner.java
@@ -0,0 +1,30 @@
+public class Lommeregner {
+
+    int plus (int a, int b) {
+        int resultat =  a + b;
+        return resultat;
+    }
+
+    int minus (int a, int b) {
+        int resultat =  a - b;
+        return resultat;
+    }
+
+    int multiply (int a, int b) {
+        int resultat =  a*b;
+        return resultat;
+    }
+
+    int dele (int a, int b) {
+        int resultat =  a/b;
+        return resultat;
+    }
+
+    int secret (int a, int b) {
+        int resultat =  a + b;
+        return resultat;
+    }
+
+
+
+}
---
+++ src/LommeregnerTest.java
@@ -0,0 +1,19 @@
+public class LommeregnerTest {
+
+    public static void main(String[] args) {
+
+        int day = 4;
+
+        if (day==1) {
+            System.out.println("Mandag");
+        } else if (day==2){
+            System.out.println("Tirsdag");
+        }
+    } else if (day==3){
+        System.out.println("Tirsdag");
+    }
+
+
+
+
+}
